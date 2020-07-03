b5945728e7c4b41617103569aa3cf68f1dfdfaa4
Zaurele <contact@aurele.eu>
2020-07-03 17:06:52 +0200

    Api fix

--- config/config.py
+++ config/config.py
@@ -253,6 +253,9 @@ class AlphaConfig():
 
         merge_configuration(config,config_env,replace=True)
 
+        if not 'root' in config:
+            config['root'] = self.root
+
         if 'users' in config:
             del config['users']
 
