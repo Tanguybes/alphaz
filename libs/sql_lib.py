e6aa46d54bfb6ee955bccf21f5616a9d87839007
Zaurele <contact@aurele.eu>
2020-10-22 13:54:14 +0200

    Clean mysql.connector

--- libs/sql_lib.py
+++ libs/sql_lib.py
@@ -1,21 +1,3 @@
-import mysql.connector
-
-class NumpyMySQLConverter(mysql.connector.conversion.MySQLConverter):
-    """ A mysql.connector Converter that handles Numpy types """
-
-    def _float32_to_mysql(self, value):
-        return float(value)
-
-    def _float64_to_mysql(self, value):
-        return float(value)
-
-    def _int32_to_mysql(self, value):
-        return int(value)
-
-    def _int64_to_mysql(self, value):
-        return int(value)
-
-
 def get_strategies_columns(db, table):
     query   = ("SHOW COLUMNS FROM %s")
     columns = db.get_query_results(query,(table),unique=True)
