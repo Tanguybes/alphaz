34824ee5c5d35fb47db6c0408e355c3a74c3cd11
Zaurele <durand.aurele@gmail.com>
2020-11-01 22:47:10 +0100

    Commit exception & configuration ini files

--- libs/config_lib.py
+++ libs/config_lib.py
@@ -1,15 +1,21 @@
-import datetime
-from collections import OrderedDict
+import datetime, os, re, configparser
 
+CONFIG_FILE_EXTENSION                   = '.ini'
+
+from collections import OrderedDict
+from alphaz.libs import converter_lib
 from ..models.database.structure import AlphaDatabase
 
+import numpy as np
 from core import core
+
+LOG = core.get_logger("config")
 
 CONSTANTS = {}
 PARAMETERS = {}
 
 def order(D: dict) -> OrderedDict:
-    return OrderedDict(sorted(values.items(), key=lambda x: len(x[0]),reverse=True))
+    return OrderedDict(sorted(D.items(), key=lambda x: len(x[0]),reverse=True))
 
 def get_db_constants(db:AlphaDatabase) -> OrderedDict:
     """Get constants from database
@@ -69,7 +75,7 @@ def get_db_constant(db:AlphaDatabase,name:str,update:bool=False):
 
     if not is_db_constant(db,name):
         get_db_constants(db)
-        if is_db_constants(db,name):
+        if is_db_constant(db,name):
             return CONSTANTS[name]
         else:
             return None
@@ -176,3 +182,155 @@ def upgrade_api_build(db:AlphaDatabase):
     """
     build = get_api_build(db)
     set_db_constant(db,'api_build',int(build) + 1)
+    
+    
+def get_config_filename(fileName,directory,test=False):
+    prefix                                      = "test_" if test else ""
+    parametersFileName                          = "%s/%s"%(directory,prefix + fileName + CONFIG_FILE_EXTENSION)
+    return parametersFileName
+    
+def get_config_from_file(fileName,directory,test=False):
+    config                                      = configparser.RawConfigParser()
+    config.optionxform                          = str
+    
+    parametersFileName                          = get_config_filename(fileName,directory=directory,test=test)
+        
+    if not os.path.isfile(parametersFileName):
+        LOG.error("Cannot find configuration file at %s (%s)"%(parametersFileName,os.getcwd()))
+        exit()
+        return None
+    
+    LOG.info("Reading config file: %s"%parametersFileName)
+    config.read(parametersFileName)
+    return config
+    
+def read_parameters_from_config(fileName,config,sectionHeader,directory,test=False):
+    parameters                                  = dict()
+
+    if not sectionHeader:
+        for section in config.sections():
+            for options in config.options(section):
+                parameters[options]             = convert_config_value(config.get(section, options).replace('\n',''))
+        
+        if test:
+            baseParameters                      = load_config_file(fileName,directory=directory,test=False)
+        
+            for baseParameter, value in baseParameters.items():
+                if not baseParameter in parameters:
+                    parameters[baseParameter]   = value
+    else:
+        for section in config.sections():
+            parameters[section] = dict()
+            for options in config.options(section):
+                value = config.get(section, options)
+                value = value.replace('\n','') if type(value) == str else value
+                parameters[section][options]    = convert_config_value(value)
+        
+        if test:
+            baseParameters                      = load_config_file(fileName,directory=directory,sectionHeader=True,test=False)
+        
+            for section, baseParameter in baseParameters.items():
+                for baseParameterName, value in baseParameter.items():
+                    if not baseParameterName in parameters[section].keys():
+                        parameters[section][baseParameterName]   = value   
+    return parameters
+    
+def load_config_file(fileName,directory,sectionHeader=False,test=False):
+    config = get_config_from_file(fileName,directory=directory,test=test)
+    if config is None:
+        return {}
+
+    return read_parameters_from_config(fileName,config,sectionHeader,directory=directory,test=False)
+        
+def convert_config_value(value):
+    if type(value) != str:
+        return value
+
+    valueStr    = str(value).upper()
+    
+    rangeMatch  = re.match('\[(.*:.*)\]', str(value))
+    listMatch   = re.match('\[(.*)\]', str(value))
+    dictMatch   = re.match('\{(.*)\}', str(value))
+
+    stringMatchQuote, stringMatch = False, False
+    if len(valueStr) != 0:
+        stringMatchQuote    = valueStr[0] == '"' and valueStr[-1] == '"' 
+        stringMatch         = valueStr[0] == "'" and valueStr[-1] == "'" if not stringMatchQuote else stringMatchQuote
+    #stringMatch = re.match('\'(.*)\'',str(value))
+        
+    if rangeMatch:
+        match       = listMatch.groups()[0]
+        values      = match.split(':')
+        step        = 1 if len(values) == 2 else convert_config_value(values[2])
+        value       = np.arange(convert_config_value(values[0]),convert_config_value(values[1])+step,step)
+    elif listMatch:
+        match       =  listMatch.groups()[0]
+        listValues  = match.split(',')
+        value       = [convert_config_value(x.lstrip()) for x in listValues]
+    elif dictMatch:
+        match       = dictMatch.groups()[0]
+        listValues  = match.split(',')
+        value       = {}
+        for el in listValues:
+            key, vl = str(el).split(':')
+            value[convert_config_value(key)] = convert_config_value(vl)
+    elif ':' in valueStr:
+        values      = valueStr.split(':')
+        step        = 1 if len(values) == 2 else convert_config_value(values[2])
+        value       = np.arange(convert_config_value(values[0]),convert_config_value(values[1])+step,step)
+    elif ',' in valueStr:
+        listValues  = valueStr.split(',')
+        value       = [convert_config_value(x.lstrip()) for x in listValues]
+    elif stringMatch:
+        #match = stringMatch.groups()[0]
+        #value = match.replace('\'','')
+        value       = value[1:-1]
+    elif valueStr == "NONE" or valueStr is None:
+        value       = None
+    elif valueStr == "FALSE":
+        value       = False
+    elif valueStr == "TRUE":
+        value       = True
+    elif valueStr == "FLOAT":
+        value       = float
+    elif valueStr == "INT":
+        value       = int
+    elif valueStr == "BOOL":
+        value       = bool
+    elif valueStr == "STR":
+        value       = str
+    else:
+        value       = converter_lib.to_num(value)
+        
+    if value is not None and type(value) == str:
+        value       = value
+    
+    return value
+
+def convert_config_value_for_write(value):
+    value               = convert_config_value(value)
+
+    if type(value) == str:
+        if not value[0] == '"' and not value[0] == "'":
+            value       = "'" + value
+        if not value[-1] == '"' and not value[-1] == "'":
+            value       += "'"
+        value           = value.replace('\n','<br>')
+    return value
+
+def write_defaults_config_files(fileName,values,directory):
+    # Add existing values
+#     existingConfig                              = load_config_file(fileName,directory=directory,sectionHeader=True)
+    existingConfig = get_config_from_file(fileName,directory=directory)
+        
+    # Set config file
+    parametersFileName                          = get_config_filename(fileName,directory=directory)
+        
+    for header, value in values.items():
+        existingConfig.set(header,'value',value)
+
+    with open(parametersFileName, 'w') as configfile:
+        existingConfig.write(configfile)
+        
+    parameters = read_parameters_from_config(fileName,existingConfig,sectionHeader=True,directory=directory,test=False)
+
