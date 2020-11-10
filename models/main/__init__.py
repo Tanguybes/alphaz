ed83dad106f4b5d7f8a0da2517edac6c83912e0c
Zaurele <durand.aurele@gmail.com>
2020-11-10 16:42:21 +0000

    Singleton and converter upgrade

--- models/main/__init__.py
+++ models/main/__init__.py
@@ -1,5 +1,5 @@
 from ._base import AlphaClass
 from ._file import AlphaFile
-from ._singleton import singleton
+from ._singleton import singleton, Singleton
 from ._exception import AlphaException
 from ._core._core import AlphaCore
