e4c99a167997a910c50c01da717469be36f9b1ae
duranda1 <aurele.durand@st.com>
2020-11-17 09:01:54 +0100

    Fix core utils

--- models/config/_utils.py
+++ models/config/_utils.py
@@ -2,6 +2,11 @@ import re, os, copy
 
 PAREMETER_PATTERN = '{{%s}}'
 
+def convert_value_for_environment(value: object) -> str:
+    if str(value).lower() == 'true': value = '1'
+    elif str(value).lower() == 'false': value = '0'
+    return str(value)
+
 def set_environment_variables(environs:dict):
     if environs:
         for key, value in environs.items():
--- models/main/_core/_utils.py
+++ models/main/_core/_utils.py
@@ -1,7 +1,4 @@
 import os
 
-def convert_value_for_environment(value: object) -> str:
-    if str(value).lower() == 'true': value = '1'
-    elif str(value).lower() == 'false': value = '0'
-    return str(value)
+
 
