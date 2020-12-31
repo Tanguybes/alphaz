2e929f9c388af7d90447a6f149afa671bc1c2ddc
Zaurele <durand.aurele@gmail.com>
2020-12-31 06:57:11 +0000

    clear user api

--- libs/notifications_lib.py
+++ libs/notifications_lib.py
@@ -1,8 +1,10 @@
 from ..models.database.main_definitions import Notification, User
 
 def add_notifications(api,db,element_type,element_action,id,users=None,all_users=True):
-    if all_users:
-        users           = db.select(User,distinct=User.id,columns=[User.id],filters=[User.id != api.user['id']])
+    user = api.get_logged_user()
 
-    notifications   = [Notification(user=x.id,user_from=api.user['id'],element_type=element_type,element_action=element_action,element_id=id) for x in users]
+    if all_users:
+        users           = db.select(User,distinct=User.id,columns=[User.id],filters=[User.id != user['id']])
+
+    notifications   = [Notification(user=x.id,user_from=user['id'],element_type=element_type,element_action=element_action,element_id=id) for x in users]
     db.add(notifications)
